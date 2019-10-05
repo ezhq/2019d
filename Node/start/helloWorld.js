399cd996fe33651f29f9f1485b45f078feb49e2f
ezhq <ezhq@outlook.com>
2019-10-05 15:42:13 +0800

    191005_154100: HelloWorld

--- Node/start/helloWorld.js
+++ Node/start/helloWorld.js
@@ -1,2 +1,11 @@
 const log = console.log.bind(console)
 
+let http = require('http')
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end('Hola!')
+}).listen(4000)
+
+log('===>Server is work on: localhost:4000')
+log('===>Press Ctrl + C to stop.')
