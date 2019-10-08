9b1490faa527ec272dd8a8baaf6b9dee13819092
ezhq <ezhq@outlook.com>
2019-10-08 13:00:28 +0800

    20191008_125900_NodePart2_END

--- Node/start/helloWorld.js
+++ Node/start/helloWorld.js
@@ -1,10 +1,42 @@
+// 引入库
+let http = require('http')
+let fs = require('fs')
+
+// 自定义函数
 const log = console.log.bind(console)
 
-let http = require('http')
+// 主函数
+function serveStaticFile(res, path, type, code) {
+    if (!code) {
+        code = 200
+    }
+    fs.readFile(__dirname + path, (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' })
+            res.end('500: Internal Error')
+        } else {
+            res.writeHead(code, { 'Content-Type': type })
+            res.end(data)
+        }
+    })
+}
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Hola!')
+    let path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
+    switch (path) {
+        case '':
+            serveStaticFile(res, '/public/home.html', 'text/html')
+            break;
+        case '/about':
+            serveStaticFile(res, '/public/about.html', 'text/html')
+            break;
+        case '/img/logo.png':
+            serveStaticFile(res, '/public/img/logo.png', 'image/png')
+            break;
+        default:
+            serveStaticFile(res, '/public/404.html', 'text/html')
+            break;
+    }
 }).listen(4000)
 
 log('===>Server is work on: localhost:4000')
